f877b053fd13d54b087271a99ba5e6bdfbb172cb
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-09-24 15:54:56 +0200

    build: Add BaseController, UserController, Database, and UserModel classes. Include necessary methods and endpoints for user listing.

---
+++ inc/config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "Your database username");
+define("DB_PASSWORD", "Your database password");
+define("DB_DATABASE_NAME", "Your database name");
